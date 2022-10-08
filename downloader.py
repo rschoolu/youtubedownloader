b7bc8a4f2cf3429ce368ce0ccd28397e6ff80448
rschoolu <smith1009tom@gmail.com>
2022-10-07 22:47:15 -0400

    fixed even more shit because i suck at everything

--- downloader.py
+++ downloader.py
@@ -1,16 +1,17 @@
 from pytube import YouTube
 import sys
-#print("Starting...")
-links = sys.argv[1].split(',')
-#print("Argument 1 Found")
-filenamee = sys.argv[2]
-audio = sys.argv[3]
-#print("Argument 2 Found")
-#print("Link valid")
-#print("Downloading..."
-def justDo():
-    global filenamee
-    global links
+def main():
+    if sys.argv[1] == "help":
+        print("Syntax: py downloader.py [yt_link] [output_name] [audio_only 0/1]")
+        return
+    #print("Starting...")
+    links = sys.argv[1].split(',')
+    #print("Argument 1 Found")
+    filenamee = sys.argv[2]
+    audio = sys.argv[3]
+    #print("Argument 2 Found")
+    #print("Link valid")
+    #print("Downloading..."
     for i in range(links.__len__()):
         name = f'{filenamee.split(".")[0]}{i}.{filenamee.split(".")[1]}'
         ytVideo = YouTube(links[i])
@@ -19,7 +20,6 @@ def justDo():
         elif audio == "1":
             ytVideo.streams.get_audio_only().download('./',filename=name)
     print("Done!")
-if links[0] == "help":
-    print("Syntax: py downloader.py [yt_link] [output_name] [audio_only 0/1]")
-else:
-    justDo()
+    return
+# main function woohoo not retarded anymore!
+main()
