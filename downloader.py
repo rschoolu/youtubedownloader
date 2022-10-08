496d0faa185ffce59e316f2211f88fc73e3232f4
rschoolu <smith1009tom@gmail.com>
2022-10-07 22:39:47 -0400

    added audio stuff and added help syntax

--- downloader.py
+++ downloader.py
@@ -4,10 +4,22 @@ import sys
 links = sys.argv[1].split(',')
 #print("Argument 1 Found")
 filenamee = sys.argv[2]
+audio = sys.argv[3]
 #print("Argument 2 Found")
 #print("Link valid")
-#print("Downloading...")
-for i in range(links.__len__()):
-    ytVideo = YouTube(links[i])
-    ytVideo.streams.get_highest_resolution().download('./',filename=f'{filenamee}{i}.mp4')
-print("Done!")
+#print("Downloading..."
+def justDo():
+    global filenamee
+    global links
+    name = f'{filenamee.split(".")[0]}{i}.{filenamee.split(".")[1]}'
+    for i in range(links.__len__()):
+        ytVideo = YouTube(links[i])
+        if audio == "0":
+            ytVideo.streams.get_highest_resolution().download('./',filename=name)
+        elif audio == "1":
+            ytVideo.streams.get_audio_only().download('./',filename=name)
+    print("Done!")
+if links[0] == "help":
+    print("Syntax: py downloader.py [yt_link] [output_name] [audio_only 0/1]")
+else:
+    justDo()
