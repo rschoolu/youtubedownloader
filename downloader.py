d5ec8f06a6b1cc12193c3da96c94e42299cac442
rschoolu <smith1009tom@gmail.com>
2022-10-16 13:25:50 -0400

    commit

--- downloader.py
+++ downloader.py
@@ -8,17 +8,12 @@ def main():
     links = sys.argv[1].split(',')
     #print("Argument 1 Found")
     filenamee = sys.argv[2]
-    audio = sys.argv[3]
     #print("Argument 2 Found")
     #print("Link valid")
     #print("Downloading..."
     for i in range(links.__len__()):
         ytVideo = YouTube(links[i])
         name = f'{filenamee.split(".")[0]}{i}.{filenamee.split(".")[1]}'
-        if audio == "0":
-            ytVideo.streams.get_highest_resolution().download('./',filename=name)
-        elif audio == "1":
-            ytVideo.streams.get_audio_only().download('./',filename=name)
     print("Done!")
     return
 # main function woohoo not retarded anymore!
